e26674fc08216aa52a10ceb24b095763fad1bcc8
Matteo <matteop18@gmail.com>
2019-12-19 15:53:06 +0100

    codice cp aggiunto

--- js/script.js
+++ js/script.js
@@ -33,17 +33,25 @@ function() {
 
   var scontoOver65 = (costoBiglietto * 40) / 100;
 
+  var carrozza = Math.floor(Math.random() *9) + 1;
+  var codiceCp = Math.floor(Math.random() * (99999 -90000 + 1)) + 90000;
+
+
   document.getElementById('nome-passeggero').innerHTML = nomeUtente;
 
   if (fasciaEta == "minorenne") {
     document.getElementById('offerta').innerHTML = "Offerta Young";
     document.getElementById('costo_ticket').innerHTML = costoBiglietto - scontoMinorenne;
+    document.getElementById('codice_cp').innerHTML = codiceCp;
   } else if (fasciaEta == "over65") {
   document.getElementById('offerta').innerHTML = "Offerta Silver";
   document.getElementById('costo_ticket').innerHTML = costoBiglietto - scontoOver65;
+  document.getElementById('codice_cp').innerHTML = codiceCp;
+
 } else {
   document.getElementById('offerta').innerHTML = "Biglietto Standard";
   document.getElementById('costo_ticket').innerHTML = costoBiglietto;
+  document.getElementById('codice_cp').innerHTML = codiceCp;
 }
 
 }
